792825756c2573997ee994b051d4d789e6ca005d
kunukulavanya03 <kunukulavanya03@gmail.com>
2025-11-28 15:26:57 +0530

    🚀 Initial commit - Generated with FigmaToFullApp

---
+++ frontend/src/pages/Blog.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from '../components/Card';
+
+function Blog() {
+  return (
+    <div>
+      <h2>Our Blog</h2>
+      <div>
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+}
+
+export default Blog;
