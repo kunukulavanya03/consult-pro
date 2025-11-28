bd319a7440336dc858434595942563cd6f150404
kunukulavanya03 <kunukulavanya03@gmail.com>
2025-11-28 15:26:54 +0530

    🚀 Initial commit - Generated with FigmaToFullApp

---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Case Studies</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Team</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
