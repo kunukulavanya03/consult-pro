da9c74f285b240d6b548a28d59cb1fa5075e675d
kunukulavanya03 <kunukulavanya03@gmail.com>
2025-11-28 15:26:55 +0530

    🚀 Initial commit - Generated with FigmaToFullApp

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from '../components/Nav';
+
+function About() {
+  return (
+    <div>
+      <Nav />
+      <h2>About Us</h2>
+      <p>We are a professional business consulting and agency</p>
+    </div>
+  );
+}
+
+export default About;
